b14482fa71f7928ad20de822ef186f8c487d0891
zhongqf <zhongqf@gmail.com>
2011-08-19 23:03:54 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camobyte::Application.config.secret_token = '275fcc0df01b81a06bb966d5c9d16060dd30971b7e2ebacfa3cff086e09f8d255d2ae998c79037fd6a277c3a56e7a4bbbe7b81d1d8eaa4fcd296be4b5aa8f8e0'
